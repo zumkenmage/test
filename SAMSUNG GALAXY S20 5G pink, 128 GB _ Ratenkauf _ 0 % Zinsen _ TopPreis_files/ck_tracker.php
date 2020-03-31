2dc53d734ce37bae8cc671a9d79a6e07c9d0ab3b
zumkenmage <62940958+zumkenmage@users.noreply.github.com>
2020-03-31 14:12:46 +0200

    1
    1sfsf

---
+++ SAMSUNG GALAXY S20 5G pink, 128 GB _ Ratenkauf _ 0 % Zinsen _ TopPreis_files/ck_tracker.php
@@ -0,0 +1,56 @@
+	(function() {
+            var google_conversion_id = 1002051394;
+            var google_conversion_label = "ULhMCO78rQcQwq7o3QM";
+            var google_custom_params = window.google_tag_params;
+            var google_remarketing_only = true;
+
+            var d = window.document.createElement("script");
+            d.src = "//www.googleadservices.com/pagead/conversion.js";
+
+            window.document.getElementsByTagName("head")[0].appendChild(d);
+	})();
+        	(function() {
+            var google_conversion_id = 1002051394;
+            var google_conversion_label = "dcYqCMbt8wcQwq7o3QM";
+            var google_custom_params = window.google_tag_params;
+            var google_remarketing_only = true;
+
+            var d = window.document.createElement("script");
+            d.src = "//www.googleadservices.com/pagead/conversion.js";
+
+            window.document.getElementsByTagName("head")[0].appendChild(d);
+	})();
+        	(function() {
+            var pxsrsc = ["https://googleads.g.doubleclick.net/pagead/viewthroughconversion/1002051394/?value=0&label=ULhMCO78rQcQwq7o3QM&guid=ON&script=0","https://googleads.g.doubleclick.net/pagead/viewthroughconversion/1002051394/?value=0&label=dcYqCMbt8wcQwq7o3QM&guid=ON&script=0","https://ad.doubleclick.net/ddm/activity/src=4744364;type=invmedia;cat=p0gts01t;ord=1?","https://ad.doubleclick.net/ddm/activity/src=5252003;type=invmedia;cat=6m9vps41;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4914505;type=invmedia;cat=7lbadhgd;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4781100;type=invmedia;cat=swsxwuxc;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://4781522.fls.doubleclick.net/activityi;src=4781522;type=invmedia;cat=zourtabe;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=6071299;type=invmedia;cat=pv1h8eks;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=6266128;type=invmedia;cat=fep8sisz;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4789111;type=invmedia;cat=uea8afph;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4781522;type=invmedia;cat=tz60cofh;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4780311;type=invmedia;cat=gozykmop;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4781710;type=invmedia;cat=0e8uvaj3;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4744364;type=invmedia;cat=hsesphjp;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=5252003;type=invmedia;cat=pvmothg5;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4798955;type=invmedia;cat=etv2debm;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4757820;type=invmedia;cat=n6n9b6lr;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=4744364;type=invmedia;cat=devwmezc;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://ad.doubleclick.net/ddm/activity/src=9677083;type=invmedia;cat=dbm-d001;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?","https://track.adform.net/Serving/TrackPoint/?pm=1351292&ADFPageName=DE%20-%20Reach%20-%20Telco&ADFdivider=|","https://ad.doubleclick.net/ddm/activity/src=9227653;type=invmedia;cat=dbm-d00;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;ord=1?","https://track.adform.net/Serving/TrackPoint/?pm=1351561&ADFPageName=Audience&ADFdivider=|","https://ad.doubleclick.net/ddm/activity/src=8293600;type=invmedia;cat=0yozjtnf;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;ord=1?","https://track.adform.net/Serving/TrackPoint/?pm=1101055&ADFPageName=Audience%20Targeting&ADFdivider=|","https://ad.doubleclick.net/ddm/activity/src=6071299;cat=erzsvikn;type=invmedia;dc_muid=<<MD5 HASH>>;ord=<<RANDOM>>","https://track.adform.net/Serving/TrackPoint/?pm=1699511&ADFPageName=AudT%20Retargetin%20Telekom%20Mobilfunk&ADFdivider=|","https://track.adform.net/Serving/TrackPoint/?pm=1351292&ADFPageName=DE-%20Reach%20-%20Telco%20ohne%20Telefonica&ADFdivider=|","https://track.adform.net/Serving/TrackPoint/?pm=1351292&ADFPageName=DE%20-%20Reach%20-%20Zanox%20-%20Technik&ADFdivider=|"];
+            var body = document.getElementsByTagName('body')[0];
+	    var px;
+
+            for(var i = 0; i < pxsrsc.length; i++) {
+                px = window.document.createElement("img");
+
+                px.style.width = "1px";
+                px.style.height = "1px";
+                px.style.border = "none";
+                px.src = pxsrsc[i];
+
+                body.appendChild(px);
+            }
+	})();
+        function magicTagLoader(magicTagId) {
+	var magicTagUrl = 'https://ad4m.at/'+magicTagId+'.js';
+        var magicTagScript = document.createElement('script');
+        magicTagScript.setAttribute('type','text/javascript');
+        magicTagScript.setAttribute('src',magicTagUrl);
+        document.getElementsByTagName('head')[0].appendChild(magicTagScript);
+	if (window.location.hostname.indexOf('webmasterplan.com') != -1) {
+	    magicTagScript.onload = function() {
+	        var pageMatch = /[\&\?]{1}(?:page\_url|product\_clickurl)\=([^\&]+)/.exec(window.location.search);
+	        advancedStoreTag.config({
+	            pageUrl: (pageMatch) ? pageMatch[1] : pageMatch
+	        });
+	    }
+	}
+}
+
+
+
